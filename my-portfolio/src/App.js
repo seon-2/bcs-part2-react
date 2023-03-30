627caa44d69693ea5d79b0f58f7a4ff16df89b46
seon-2 <phia130@naver.com>
2023-03-30 16:38:15 +0900

    💄 [design] 포트폴리오 카드 여러개

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -29,13 +29,50 @@ function App() {
         <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex">
           내 소개
         </div>
-        <ul className="bg-indigo-100 max-w-screen-xl mx-auto">
+        {/* 포트폴리오 카드 */}
+        {/* 그리드 줄에 2개씩 / 여백은 gap, p(padding) / sm-lg, 1-4까지 크기에 따라 반응형 줄 수 있음*/}
+        <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 mg:grid-cols-3 lg:grid-cols-4 justify-items-center gap-8 py-8">
           <li className="bg-purple-200 w-60 h-72 rounded-xl">
             {/* 높이 절반, 이미지 들어오면 object-fit : cover, 위에만 둥글게 (-t) */}
             <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
               프로젝트 이미지
             </div>
             {/* 텍스트 사이즈, 굵기, 위쪽(top), x축에 (왼, 오 모두) */}
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 설명</div>
+          </li>
+          <li className="bg-purple-200 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 설명</div>
+          </li>
+          <li className="bg-purple-200 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 설명</div>
+          </li>
+          <li className="bg-purple-200 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 설명</div>
+          </li>
+          <li className="bg-purple-200 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 설명</div>
+          </li>
+          <li className="bg-purple-200 w-60 h-72 rounded-xl">
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
             <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
             <div className="mt-2 mx-2">프로젝트 설명</div>
           </li>
