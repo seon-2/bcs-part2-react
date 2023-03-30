784e49105e96d235de7367216600a03b9d26e480
seon-2 <phia130@naver.com>
2023-03-30 11:35:42 +0900

    💄 [design] className 별로 css 미리 만들어 적용

--- dapp/src/App.jsx
+++ dapp/src/App.jsx
@@ -4,9 +4,13 @@ function App() {
   return (
     <div>
       {/* jsx에서는 class 대신 className 사용 */}
-      <h1 className="hello">hello react!</h1>
-      <h2 className="name">My name is JISEON</h2>
-      <h3 className="favorite">My favorite flower is tulip.🌷</h3>
+      <h1 className="p-12 br-12 inline-block bg-c-lightblue">hello react!</h1>
+      <h2 className="p-12 br-12 inline-block m-l-12 bg-c-lightgray">
+        My name is JISEON
+      </h2>
+      <h3 className="p-12 br-12 inline-block m-l-12 bg-c-lightcoral ">
+        My favorite flower is tulip.🌷
+      </h3>
     </div>
   );
 }
