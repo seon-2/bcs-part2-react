967d26f9dce3e92676a54910be5fb3dfd77bcb2c
seon-2 <phia130@naver.com>
2023-03-30 16:46:18 +0900

    💄 [design] footer 부분

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -81,6 +81,9 @@ function App() {
           내 연락처
         </div>
       </main>
+      <footer className="max-w-screen-xl mx-auto flex justify-end py-1 md:py-2">
+        2023, designed by, jiseon
+      </footer>
     </div>
   );
 }
