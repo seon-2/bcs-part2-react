be3169d6a1f7d3ae0a8fb33d98a9baf27a242698
seon-2 <phia130@naver.com>
2023-03-30 11:55:31 +0900

    💄 [design] tailwind CSS 적용

--- dapp/src/App.jsx
+++ dapp/src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
+// import "./index.css";
 
 function App() {
   return (
     <div>
       {/* jsx에서는 class 대신 className 사용 */}
-      <h1 className="p-12 br-12 inline-block bg-c-lightblue">hello react!</h1>
-      <h2 className="p-12 br-12 inline-block m-l-12 bg-c-lightgray">
-        My name is JISEON
-      </h2>
-      <h3 className="p-12 br-12 inline-block m-l-12 bg-c-lightcoral ">
+      <h1 className="text-3xl font-bold underline">hello react!</h1>
+      <h2 className="text-3xl font-bold underline">My name is JISEON</h2>
+      <h3 className="text-3xl font-bold underline">
         My favorite flower is tulip.🌷
       </h3>
     </div>
