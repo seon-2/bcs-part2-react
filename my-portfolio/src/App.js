6673699cdcc81a0e528a2ef63d037f98a81f2c6e
seon-2 <phia130@naver.com>
2023-03-30 17:12:44 +0900

    💄 [design] 설정한 main 색상 적용

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -81,7 +81,7 @@ function App() {
           내 연락처
         </div>
       </main>
-      <footer className="max-w-screen-xl mx-auto flex justify-end py-1 md:py-2">
+      <footer className="max-w-screen-xl mx-auto flex justify-end py-1 md:py-2 text-main-200">
         2023, designed by, jiseon
       </footer>
     </div>
