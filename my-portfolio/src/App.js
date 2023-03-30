b577917d2b467fb90867361fb812d997730f52a4
seon-2 <phia130@naver.com>
2023-03-30 16:24:00 +0900

    💄 [design] 포트폴리오 카드 디자인

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -29,6 +29,17 @@ function App() {
         <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex">
           내 소개
         </div>
+        <ul className="bg-indigo-100 max-w-screen-xl mx-auto">
+          <li className="bg-purple-200 w-60 h-72 rounded-xl">
+            {/* 높이 절반, 이미지 들어오면 object-fit : cover, 위에만 둥글게 (-t) */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            {/* 텍스트 사이즈, 굵기, 위쪽(top), x축에 (왼, 오 모두) */}
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 설명</div>
+          </li>
+        </ul>
       </main>
     </div>
   );
