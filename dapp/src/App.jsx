e9520111598ed9211a1b67421fba84a2dcd636d5
seon-2 <phia130@naver.com>
2023-03-30 11:24:24 +0900

    💄 [design] App.css 적용

--- dapp/src/App.jsx
+++ dapp/src/App.jsx
@@ -1,8 +1,12 @@
+import "./App.css";
+
 function App() {
   return (
     <div>
-      <h1>hello react!</h1>
-      <h2>My name is JISEON</h2>
+      {/* jsx에서는 class 대신 className 사용 */}
+      <h1 className="hello">hello react!</h1>
+      <h2 className="name">My name is JISEON</h2>
+      <h3 className="favorite">My favorite flower is tulip.🌷</h3>
     </div>
   );
 }
