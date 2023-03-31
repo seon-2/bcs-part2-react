5013996dee26add2b696514bacc8594421da7bfa
seon-2 <phia130@naver.com>
2023-03-31 14:20:14 +0900

    ✨ [feature] 컴포넌트 분리 (B, C)

---
+++ dapp-03-31/src/components/B.jsx
@@ -0,0 +1,19 @@
+// 파일 위치 잘 확인하기! 작업 폴더/src/components/B.jsx
+const B = () => {
+  // return 안에는 html이 들어갑니다!
+  return (
+    <div className="bg-green-300 w-full h-80">
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      {/*  */}2{/*  */}
+      {/*  */}
+      {/*  */}
+      {/*  */}
+    </div>
+  );
+};
+export default B;
+// 파일 이름과 함수 이름 같게 해주는 게 관례! (그렇게 많이 씀, 파일 하나 당 함수 하나)
+// export default B <- 여기 B는 함수 이름! 해당 함수를 내보낸다는 의미
+// B.jsx, const B () => {}, export default B
---
+++ dapp-03-31/src/components/C.jsx
@@ -0,0 +1,19 @@
+// 파일 위치 잘 확인하기! 작업 폴더/src/components/C.jsx
+const C = () => {
+  // return 안에는 html이 들어갑니다!
+  return (
+    <div className="bg-purple-300 w-full h-80">
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      {/*  */}3{/*  */}
+      {/*  */}
+      {/*  */}
+      {/*  */}
+    </div>
+  );
+};
+export default C;
+// 파일 이름과 함수 이름 같게 해주는 게 관례! (그렇게 많이 씀, 파일 하나 당 함수 하나)
+// export default C <- 여기 A는 함수 이름! 해당 함수를 내보낸다는 의미
+// C.jsx, const C () => {}, export default C
