2b8521ea44c6a4e8557815cc0e6b7d4856d66bdf
seon-2 <phia130@naver.com>
2023-03-31 10:10:37 +0900

    🌷 [explain] useState 사용
    초기값(useState(2)) 변경해서 화면에서 현재값(count) 변경 되는 것 확인

--- dapp-03-31/src/App.js
+++ dapp-03-31/src/App.js
@@ -1,19 +1,14 @@
+import { useState } from "react";
+
 function App() {
-  let noUseState = 0;
+  const [count, setCount] = useState(2);
 
-  noUseState = 1;
-
-  const onClickAdd = () => {
-    // 똑같이 변수에 1씩 더하는 코드
-    // noUseState = noUseState + 1;
-    // noUseState += 1;
-    noUseState++;
-    // 버튼 눌러도 화면에서 숫자 증가 안함
-  };
+  const onClickAdd = () => {};
 
   return (
     <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
-      <div>{noUseState}</div>
+      {/* html 영역에서 js 변수 사용하려면 {} 사용! */}
+      <div>{count}</div>
       <button
         className="bg-green-300 w-12 h-12 rounded-md"
         onClick={onClickAdd}
