3b602cafa8e9cc997a2508029c2e8f885a7275e2
seon-2 <phia130@naver.com>
2023-03-31 14:39:00 +0900

    🌷 [explain] Header.jsx에 header 부분 붙여넣기

--- dapp-03-31/src/components/Header.jsx
+++ dapp-03-31/src/components/Header.jsx
@@ -1,4 +1,20 @@
 const Header = () => {
-  return;
+  return (
+    <header className="bg-blue-100 h-12 md:h-20">
+      <div className="max-w-screen-xl mx-auto h-full flex justify-between items-center">
+        {/* <div>jiseon's portfolio</div> */}
+        <div>
+          {/* 로고이미지로 교체 가능 */}
+          {/* <img className="w-12" src={SunriseLogo} alt="SunriseLogo"></img> */}
+          <div>jiseon's portfolio</div>
+        </div>
+        <ul className="flex gap-2 md:gap-8 text-xs md:text-base">
+          <li className="bg-indigo-300 btn-style">Introduce</li>
+          <li className="bg-purple-300 btn-style">Portfolio</li>
+          <li className="bg-blue-300 btn-style">Contact Me</li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 export default Header;
