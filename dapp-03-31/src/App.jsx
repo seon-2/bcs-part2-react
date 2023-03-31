4be8a563f070c0fcbb17c689b5e5130bd31e861b
seon-2 <phia130@naver.com>
2023-03-31 10:35:31 +0900

    :broom: [clean-up]

--- dapp-03-31/src/App.jsx
+++ dapp-03-31/src/App.jsx
@@ -1,44 +1,7 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  const onClickAdd = () => {
-    setCount(count + 1);
-  };
-
-  const onClickSubtract = () => {
-    // 이전 값을 가져와서 사용
-    /* 여러 줄 주석
-    setCount((prev) => {
-      return prev - 1;
-    });
-    */
-
-    if (count <= 0) {
-      alert("count의 값이 0보다 작을 수 없습니다!");
-      return;
-    }
-
-    setCount(count - 1);
-  };
-
   return (
     <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
-      {/* html 영역에서 js 변수 사용하려면 {} 사용! */}
-      <div>{count}</div>
-      <button
-        className="bg-green-300 w-12 h-12 rounded-md"
-        onClick={onClickAdd}
-      >
-        +
-      </button>
-      <button
-        className="bg-red-300 w-12 h-12 rounded-md mt-4"
-        onClick={onClickSubtract}
-      >
-        -
-      </button>
+      Hello, React!
     </div>
   );
 }
