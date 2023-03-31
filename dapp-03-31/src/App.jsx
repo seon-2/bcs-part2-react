83f1c770dd4f731caaba3bf4fa480b8145c452f2
seon-2 <phia130@naver.com>
2023-03-31 14:06:50 +0900

    💄 [design] 화면 3부분으로 나누기

--- dapp-03-31/src/App.jsx
+++ dapp-03-31/src/App.jsx
@@ -1,6 +1,34 @@
 function App() {
   return (
-    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center"></div>
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      <div className="bg-blue-300 w-full h-80">
+        {/*  */}
+        {/*  */}
+        {/*  */}
+        {/*  */}1{/*  */}
+        {/*  */}
+        {/*  */}
+        {/*  */}
+      </div>
+      <div className="bg-green-300 w-full h-80">
+        {/*  */}
+        {/*  */}
+        {/*  */}
+        {/*  */}2{/*  */}
+        {/*  */}
+        {/*  */}
+        {/*  */}
+      </div>
+      <div className="bg-purple-300 w-full h-80">
+        {/*  */}
+        {/*  */}
+        {/*  */}
+        {/*  */}3{/*  */}
+        {/*  */}
+        {/*  */}
+        {/*  */}
+      </div>
+    </div>
   );
 }
 
