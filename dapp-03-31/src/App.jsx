ea96d308392a2d1571b86e75e9d9a1c74e656a16
seon-2 <phia130@naver.com>
2023-03-31 14:41:33 +0900

    🌷 [explain] App.jsx에 Header 컴포넌트 넣기

--- dapp-03-31/src/App.jsx
+++ dapp-03-31/src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="bg-red-100 min-h-screen">
-      
+      <Header />
       <main>
         {/* Introduce */}
         <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex">
