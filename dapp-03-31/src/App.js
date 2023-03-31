b15076d383165c9ebc6a6cafabc6bbeae17728f4
seon-2 <phia130@naver.com>
2023-03-31 09:33:24 +0900

    💄 [design] 버튼 만들고 숫자 표시(noUseState)

--- dapp-03-31/src/App.js
+++ dapp-03-31/src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <div className="bg-red-100">Hello, React!</div>;
+  let noUseState = 0;
+
+  return (
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      <div>{noUseState}</div>
+      <button className="bg-green-300 w-12 h-12 rounded-md">+</button>
+    </div>
+  );
 }
 
 export default App;
