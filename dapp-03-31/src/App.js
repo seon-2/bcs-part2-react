fafecc5f7718b9c8a10f75327b5f5fde0546c4d2
seon-2 <phia130@naver.com>
2023-03-31 09:38:47 +0900

    ✨ [feature] onClickAdd 함수 만들어서 적용

--- dapp-03-31/src/App.js
+++ dapp-03-31/src/App.js
@@ -1,14 +1,18 @@
 function App() {
   let noUseState = 0;
 
+  noUseState = 1;
+
+  const onClickAdd = () => {
+    console.log("clicked!");
+  };
+
   return (
     <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
       <div>{noUseState}</div>
       <button
         className="bg-green-300 w-12 h-12 rounded-md"
-        onClick={() => {
-          console.log("clicked!");
-        }}
+        onClick={onClickAdd}
       >
         +
       </button>
