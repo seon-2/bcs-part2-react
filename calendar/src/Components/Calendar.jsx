e011c5d42ad5a22f5b37bd478d194f7199075958
seon-2 <phia130@naver.com>
2023-04-03 11:44:47 +0900

    💄 [design] Calendar 에 날짜 추가

--- calendar/src/Components/Calendar.jsx
+++ calendar/src/Components/Calendar.jsx
@@ -3,6 +3,7 @@ import Date from "./Date";
 function Calendar() {
   return (
     <div className="bg-white w-1/2 rounded-lg shadow-2xl p-8">
+      <div className="pb-8 text-4xl font-bold">2023년 04월</div>
       <Date />
     </div>
   );
