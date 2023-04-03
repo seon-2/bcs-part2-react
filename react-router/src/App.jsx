0a0feba9df010afaf07b57cdb22b5cf329261c8d
seon-2 <phia130@naver.com>
2023-04-03 16:14:56 +0900

    💄 [design] router 사용해 페이징 구현

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import A from "./pages/a";
+import B from "./pages/b";
+import C from "./pages/c";
+import Main from "./pages/main";
+import NotFound from "./pages/notFound";
+
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  return (
+    <BrowserRouter>
+      <div className="bg-red-100 min-h-screen">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="a" element={<A />} />
+          <Route path="b" element={<B />} />
+          <Route path="c" element={<C />} />
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
