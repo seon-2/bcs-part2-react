8b55305ee60740563449de26d61943d9a90eefbd
seon-2 <phia130@naver.com>
2023-04-03 10:24:56 +0900

    🌷 [explain] 배열 구조분해

--- props/a.js
+++ props/a.js
@@ -1,16 +1,12 @@
 // 구조분해 실습 a.js
-// console.log("hello, nodeJS!");
 
 // 배열 하나 만들기
 const fruits = ["Apple", "Banana", "Cherry"];
-// console.log(fruits);
 
-// 하나씩 접근할 때는
-// console.log(fruits[0]);
-const A = fruits[0];
-const B = fruits[1];
-const C = fruits[2];
+let a, b, c;
 
-console.log(A);
-console.log(B);
-console.log(C);
+[a, b, c] = fruits;
+
+console.log(a);
+console.log(b);
+console.log(c);
