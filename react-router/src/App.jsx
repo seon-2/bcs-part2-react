7bf2ac5cfddcb2b57ab92d4420339195e90495e8
seon-2 <phia130@naver.com>
2023-04-03 16:28:20 +0900

    💄 [design] 헤더, 푸터 모든 페이지에 적용

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,22 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import A from "./pages/a";
 import B from "./pages/b";
 import C from "./pages/c";
 import Main from "./pages/main";
 import NotFound from "./pages/notFound";
 
-
 function App() {
   return (
     <BrowserRouter>
-      <div className="bg-red-100 min-h-screen">
+      <div className="min-h-screen flex flex-col">
+        <Header />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="a" element={<A />} />
           <Route path="b" element={<B />} />
           <Route path="c" element={<C />} />
-          <Route path="*" element={<NotFound/>} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );
