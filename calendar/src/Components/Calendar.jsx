6f6c93a9eb2632e16204424d521361fb04861447
seon-2 <phia130@naver.com>
2023-04-03 13:15:04 +0900

    ✨ [feature] map 으로 Date 30개 출력

--- calendar/src/Components/Calendar.jsx
+++ calendar/src/Components/Calendar.jsx
@@ -1,10 +1,13 @@
 import Date from "./Date";
+import calendarData from "../calendarData.json";
 
 function Calendar() {
   return (
     <div className="bg-white w-1/2 rounded-lg shadow-2xl p-8">
       <div className="pb-8 text-4xl font-bold">2023년 04월</div>
-      <Date />
+      {calendarData.map((v, i) => { // v는 value, i는 index (파라미터이기 때문에 변수는 마음대로 해도 됨!)
+        return <Date />;
+      })}
     </div>
   );
 }
