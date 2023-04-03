30a4ad5600161e3256ee4d6ccfa75ac9b2f266fc
seon-2 <phia130@naver.com>
2023-04-03 13:50:50 +0900

    🌷 [explain] StrictMode

--- calendar/src/Components/Calendar.jsx
+++ calendar/src/Components/Calendar.jsx
@@ -7,7 +7,7 @@ function Calendar() {
       <div className="pb-8 text-4xl font-bold">2023년 04월</div>
       {calendarData.map((v, i) => {
         // v는 value, i는 index (파라미터이기 때문에 변수는 마음대로 해도 됨!)
-        // index와 value 콘솔에 출력해보기 (콘솔에 2번 찍히지만, 이거는 나중에)
+        // index와 value 콘솔에 출력해보기 (콘솔에 2번 찍히지만, 이거는 -> 디버깅 렌더링 때문에! (index.js의 React.StrictMode) 실습 시 끄지 않는 것이 좋음_디버깅)
         // -> Warning: Each child in a list should have a unique "key" prop. 에러 확인!
         console.log(`index : ${i + 1}, value : ${v}`);
         return <Date key={i} />; // key prop 추가해서 에러 해결!
