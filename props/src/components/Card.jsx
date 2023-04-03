7d5e7481d85cb3b80a6924a81cf41cf586790beb
seon-2 <phia130@naver.com>
2023-04-03 10:11:23 +0900

    🌷 [explain] props - 구조분해 적용

--- props/src/components/Card.jsx
+++ props/src/components/Card.jsx
@@ -1,6 +1,6 @@
-function Card(props) {
+function Card({ color, cardName }) {
   // App.jsx(부모)로부터 값을 내려받을 수 있음
-  return <div className={`${props.color} w-40 h-60`}>{props.cardName}</div>;
+  return <div className={`${color} w-40 h-60`}>{cardName}</div>;
 }
 
 export default Card;
