067c63aacda7ba7f4f3f4b8b688ce11b633dee48
seon-2 <phia130@naver.com>
2023-04-03 10:08:50 +0900

    💄 [design] props 사용해서 Card 여러 개 만들기

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -5,6 +5,11 @@ function App() {
     <div className="min-h-screen p-12 grid grid-cols-3">
       {/* 카드(자식)에게 넘겨줄 값 작성 */}
       <Card cardName="A" color="bg-red-100" />
+      <Card cardName="B" color="bg-orange-100" />
+      <Card cardName="C" color="bg-yellow-100" />
+      <Card cardName="D" color="bg-green-100" />
+      <Card cardName="E" color="bg-blue-100" />
+      <Card cardName="F" color="bg-purple-100" />
     </div>
   );
 }
