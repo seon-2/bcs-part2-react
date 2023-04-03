510127decda4713d82dc6e2ac333b125413c8214
seon-2 <phia130@naver.com>
2023-04-03 10:43:54 +0900

    🌷 [explain] 객체 구조분해

--- props/a.js
+++ props/a.js
@@ -8,6 +8,14 @@ const myProfile = {
   isGenius: true,
 };
 
-console.log(myProfile);
+// console.log(myProfile);
 // 이름만 출력하려면
-console.log(myProfile.name);
+// console.log(myProfile.name);
+
+// 객체 구조분해
+// 배열은 다 꺼냈지만, 객체는 필요한 것만 꺼냄.
+// 배열은 순서가 정해져있지만, 객체는 순서가 없고 key와 value 존재!
+const { name, age } = myProfile;
+// const { age, name } = myProfile;  // 정상작동!
+console.log(name); // js에서 이미 존재하는 키워드가 있음. 
+console.log(age);
