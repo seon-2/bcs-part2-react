c67b2c2eb6de887b8ee2b378541390b93fd5387b
seon-2 <phia130@naver.com>
2023-04-03 14:17:54 +0900

    ✨ [feature] Date props 적용

--- calendar/src/Components/Date.jsx
+++ calendar/src/Components/Date.jsx
@@ -1,12 +1,15 @@
-function Date() {
+function Date(props) {
+  console.log(props);
+
   return (
     <li className="inline-block bg-white rounded-lg shadow-md">
       <div className="bg-red-400 rounded-t-lg px-2 py-1 text-xl text-white">
-        2
+        {props.date}
       </div>
       <ul className="p-2 text-lg">
-        <li>🧹 청소하기</li>
-        <li>👕 빨래하기</li>
+        {props.todos.map((v, i) => {
+          return <li>{v}</li>;
+        })}
       </ul>
     </li>
   );
