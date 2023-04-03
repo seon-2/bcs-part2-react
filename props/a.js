88c7ec9bace530601176f126dd151a94606e63fc
seon-2 <phia130@naver.com>
2023-04-03 11:02:03 +0900

    🌷 [explain] 구조분해가 필요 없는 경우

--- props/a.js
+++ props/a.js
@@ -1,21 +1,16 @@
 // 구조분해 실습 a.js
 
-// 객체 하나 만들기
-const myProfile = {
-  name: "jiseon",
-  age: 999,
-  job: "student",
-  isGenius: true,
+// 구조분해가 필요 없는 경우
+const seon2 = {
+  nickName: "seon-2",
+};
+const seon3 = {
+  nickName: "seon-3",
 };
 
-// console.log(myProfile);
-// 이름만 출력하려면
-// console.log(myProfile.name);
+const { nickName } = seon2;
+// const { nickName } = seon3; // 오류 발생!! 키 값이 같음
+// console.log(seon3.nickName);
+const { nickName: newNickName } = seon3; // 키 값 바꾸기
 
-// 객체 구조분해
-// 배열은 다 꺼냈지만, 객체는 필요한 것만 꺼냄.
-// 배열은 순서가 정해져있지만, 객체는 순서가 없고 key와 value 존재!
-const { name, age } = myProfile;
-// const { age, name } = myProfile;  // 정상작동!
-console.log(name); // js에서 이미 존재하는 키워드가 있음. 
-console.log(age);
+console.log(newNickName);
