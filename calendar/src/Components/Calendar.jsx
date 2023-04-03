065de2e41063668f8df885e5fd1d504e4d3eddbb
seon-2 <phia130@naver.com>
2023-04-03 11:41:07 +0900

    💄 [design] Date 컴포넌트 생성 및 적용
    Calendar 컴포넌트에 적용!!

--- calendar/src/Components/Calendar.jsx
+++ calendar/src/Components/Calendar.jsx
@@ -1,6 +1,10 @@
+import Date from "./Date";
+
 function Calendar() {
   return (
-    <div className="bg-white w-1/2 rounded-lg shadow-2xl p-8">Calendar</div>
+    <div className="bg-white w-1/2 rounded-lg shadow-2xl p-8">
+      <Date />
+    </div>
   );
 }
 
---
+++ calendar/src/Components/Date.jsx
@@ -0,0 +1,15 @@
+function Date() {
+  return (
+    <li className="inline-block bg-white rounded-lg shadow-md">
+      <div className="bg-red-400 rounded-t-lg px-2 py-1 text-xl text-white">
+        2
+      </div>
+      <ul className="p-2 text-lg">
+        <li>🧹 청소하기</li>
+        <li>👕 빨래하기</li>
+      </ul>
+    </li>
+  );
+}
+
+export default Date;
