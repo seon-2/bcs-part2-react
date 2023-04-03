9b9ac95ce283f46f6325363c7e0e1370678f7051
seon-2 <phia130@naver.com>
2023-04-03 14:26:00 +0900

    ✨ [feature] 요일별 색상 - 삼항연산자 사용 (일요일 빨간색)

--- calendar/src/Components/Date.jsx
+++ calendar/src/Components/Date.jsx
@@ -3,7 +3,13 @@ function Date(props) {
 
   return (
     <li className="inline-block bg-white rounded-lg shadow-md">
-      <div className="bg-red-400 rounded-t-lg px-2 py-1 text-xl text-white">
+      {/* {``} 백틱으로 바꾸면 js 코드 사용 가능 */}
+      {/* 삼항연산자 [조건] ? 참이면 실행될 코드 : 거짓이면 실행될 코드*/}
+      <div
+        className={`${
+          props.day === "SUN" ? "bg-red-400" : "bg-blue-400"
+        } rounded-t-lg px-2 py-1 text-xl text-white`}
+      >
         {props.date}
       </div>
       <ul className="p-2 text-lg">
