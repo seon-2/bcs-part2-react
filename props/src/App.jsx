22606c90b9afb3f8a7e9281c9c2b906c57ade7ae
seon-2 <phia130@naver.com>
2023-04-03 10:07:32 +0900

    🌷 [explain] props 사용하기

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -3,7 +3,8 @@ import Card from "./components/Card";
 function App() {
   return (
     <div className="min-h-screen p-12 grid grid-cols-3">
-      <Card />
+      {/* 카드(자식)에게 넘겨줄 값 작성 */}
+      <Card cardName="A" color="bg-red-100" />
     </div>
   );
 }
--- props/src/components/Card.jsx
+++ props/src/components/Card.jsx
@@ -1,5 +1,6 @@
-function Card() {
-  return <div className="bg-red-100 w-40 h-60">A</div>;
+function Card(props) {
+  // App.jsx(부모)로부터 값을 내려받을 수 있음
+  return <div className={`${props.color} w-40 h-60`}>{props.cardName}</div>;
 }
 
 export default Card;
