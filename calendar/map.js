5f85adad5bf130e12098c77aa7ec5083deb3f797
seon-2 <phia130@naver.com>
2023-04-03 13:36:30 +0900

    🌷 [explain] map 함수 실습 - 새로운 배열 생성

--- calendar/map.js
+++ calendar/map.js
@@ -10,6 +10,27 @@ for (let i = 0; i < fruits.length; i++) {
 }
 
 // map 함수 사용
-fruits.map((value, index) => {
-  console.log(`${index + 1}번째 과일 : ${value}`);
-});
+// fruits.map((value, index) => {
+//   console.log(`${index + 1}번째 과일 : ${value}`);
+// });
+
+// map 함수는 값을 받아서 복작복작 한 후, 새로운 배열을 생성할 수 있음
+const array = [1, 2, 4, 8];
+// console.log(array);
+
+// const squareArray = array.map((v) => {
+//   return v * v;
+// });
+// console.log(squareArray);
+// map 함수 한 줄로
+const squareArray = array.map((v) => v * v);
+
+// for문으로 똑같이 만들어보기
+const squareArray2 = [];
+
+for (let i = 0; i < array.length; i++) {
+  squareArray2.push(array[i] * array[i]);
+}
+
+console.log(squareArray);
+console.log(squareArray2);
