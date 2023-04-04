89b6eeb46b3d96e3c72f77b5ad76121a65baf738
seon-2 <phia130@naver.com>
2023-04-04 09:39:13 +0900

    ✨ [feature] map 으로 카드 데이터 적용

--- card/src/App.jsx
+++ card/src/App.jsx
@@ -1,9 +1,12 @@
 import Card from "./components/Card";
+import cardData from "./cardData.json";
 
 function App() {
   return (
     <div className="bg-red-100">
-      <Card />
+      {cardData.map((v, i) => {
+        return <Card key={i} symbol={v.symbol} score={v.score} />;
+      })}
     </div>
   );
 }
