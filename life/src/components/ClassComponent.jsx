caa6d1fc96ed287ce895d9e3aa52fd5c7830696d
seon-2 <phia130@naver.com>
2023-04-04 11:07:34 +0900

    🌷 [explain] 함수형, 클래스형 설명

--- life/src/components/ClassComponent.jsx
+++ life/src/components/ClassComponent.jsx
@@ -1,5 +1,8 @@
 import { Component } from "react";
 
+// extends Component : Component에 들어있는 모든 기능들을 가져와서 쓰겠다
+// Component는 라이프사이클 기능을 포함하고 있다. (index.d.ts의 interface ComponentLifecycle)
+// 즉, 클래스컴포넌트는 라이프사이클 기능을 가진다!
 class ClassComponent extends Component {
   render() {
     return <div className="bg-yellow-100 h-96">ClassComponent</div>;
--- life/src/components/FunctionalComponent.jsx
+++ life/src/components/FunctionalComponent.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 const FunctionalComponent = () => {
+  // 필요한 기능 하나만 가져와서 사용
+  const [count, setCount] = useState(0);
+  
   return <div className="bg-blue-100 h-96">FunctionalComponent</div>;
 };
 
