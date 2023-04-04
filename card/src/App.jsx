c7be88fcc8c6926274b8b2d860c35d0617bfb9da
seon-2 <phia130@naver.com>
2023-04-04 09:40:58 +0900

    💄 [design] 화면 디자인 코드 추가

--- card/src/App.jsx
+++ card/src/App.jsx
@@ -3,7 +3,7 @@ import cardData from "./cardData.json";
 
 function App() {
   return (
-    <div className="bg-red-100">
+    <div className="bg-red-100 grid grid-cols-4 gap-8 py-8 justify-items-center">
       {cardData.map((v, i) => {
         return <Card key={i} symbol={v.symbol} score={v.score} />;
       })}
