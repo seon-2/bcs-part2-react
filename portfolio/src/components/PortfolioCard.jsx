19e4d20d25b52c46a67b3f069f61e9c63271ccbb
seon-2 <phia130@naver.com>
2023-04-04 15:41:42 +0900

    💄 [design] 이미지 가져오기

--- portfolio/src/components/PortfolioCard.jsx
+++ portfolio/src/components/PortfolioCard.jsx
@@ -11,7 +11,12 @@ function PortfolioCard({ title, desc, image }) {
   return (
     <li className="bg-purple-100 w-60 h-72 rounded-xl">
       <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
-        프로젝트 이미지
+        <img
+          className="w-full h-full object-cover"
+          src={`${process.env.PUBLIC_URL}/${image}`}
+          alt="projectImg"
+        />
+        {/* public 경로에서 이미지 불러오는 방법 (json 파일에 img 경로 추가해줘함) */}
       </div>
       <div className="text-xl font-bold mt-2 mx-2">{title}</div>
       <div className="mt-2 mx-2">{desc}</div>
