f292ac590ead47582c6f91a4c867d3617326ab60
seon-2 <phia130@naver.com>
2023-04-04 11:24:03 +0900

    💄 [design] Counter 컴포넌트 생성 및 적용

--- effect/src/App.jsx
+++ effect/src/App.jsx
@@ -1,5 +1,14 @@
+import Counter from "./components/Counter";
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  return (
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center">
+      <Counter />
+      <button className="bg-pink-300 px-4 py-2 rounded-2xl text-white font-bold mt-4">
+        버튼
+      </button>
+    </div>
+  );
 }
 
 export default App;
