355388053eba9822fd0a80bb016bf881803e363d
seon-2 <phia130@naver.com>
2023-04-04 15:35:56 +0900

    💄 [design] props 적용 (title, desc)

--- portfolio/src/components/PortfolioCard.jsx
+++ portfolio/src/components/PortfolioCard.jsx
@@ -1,14 +1,20 @@
-function PortfolioCard() {
+import { useEffect } from "react";
+
+function PortfolioCard({ title, desc, image }) {
+  // Portfolio에서 props로 값 내려받아 적용
+  useEffect(() => {
+    console.log(title);
+    console.log(desc);
+    console.log(image);
+  }, []);
+
   return (
     <li className="bg-purple-100 w-60 h-72 rounded-xl">
       <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
         프로젝트 이미지
       </div>
-      <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
-      <div className="mt-2 mx-2">
-        갑 든 예가 사막이다. 수 가진 고행을 따뜻한 곳으로 천고에 피다. 눈에
-        미인을 때까지 굳세게 그들의 바이며,
-      </div>
+      <div className="text-xl font-bold mt-2 mx-2">{title}</div>
+      <div className="mt-2 mx-2">{desc}</div>
     </li>
   );
 }
