6e93902593ced884a8c968129fe86325ed12f4c5
seon-2 <phia130@naver.com>
2023-04-04 14:29:35 +0900

    ✨ [feature] counter 기능 구현_useState

--- effect/src/components/Counter.jsx
+++ effect/src/components/Counter.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const Counter = () => {
-  return <div>Counter</div>;
+  const [count, setCount] = useState(0);
+
+  const onClickAdd = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="text-center text-4xl">{count}</div>
+      <button
+        onClick={onClickAdd}
+        className="bg-red-500 px-4 py-2 rounded-2xl text-white font-bold mt-4"
+      >
+        +
+      </button>
+    </div>
+  );
 };
 
 export default Counter;
