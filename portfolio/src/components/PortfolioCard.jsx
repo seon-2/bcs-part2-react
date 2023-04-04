eef2735321d05b1eaf424d771d89bb16119442d3
seon-2 <phia130@naver.com>
2023-04-04 15:25:29 +0900

    🎉 [start] 프로젝트 시작_portfolio

---
+++ portfolio/src/components/PortfolioCard.jsx
@@ -0,0 +1,16 @@
+function PortfolioCard() {
+  return (
+    <li className="bg-purple-100 w-60 h-72 rounded-xl">
+      <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+        프로젝트 이미지
+      </div>
+      <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+      <div className="mt-2 mx-2">
+        갑 든 예가 사막이다. 수 가진 고행을 따뜻한 곳으로 천고에 피다. 눈에
+        미인을 때까지 굳세게 그들의 바이며,
+      </div>
+    </li>
+  );
+}
+
+export default PortfolioCard;
