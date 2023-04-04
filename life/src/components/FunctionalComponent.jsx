4e22d585559eaed336f0c492901d738f56e3f786
seon-2 <phia130@naver.com>
2023-04-04 11:14:05 +0900

    🌷 [explain] useEffect 간단 설명(포맷)

--- life/src/components/FunctionalComponent.jsx
+++ life/src/components/FunctionalComponent.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useEffect } from "react";
 
 const FunctionalComponent = () => {
-  // 필요한 기능 하나만 가져와서 사용
-  const [count, setCount] = useState(0);
-  
+  // useEffect 라이프사이클을 함수형 컴포넌트에서 구현하기 위해서! (cf. 상태관리는 useState!)
+  /*
+  (alias) useEffect(effect: React.EffectCallback, deps?: 
+    React.DependencyList | undefined): void
+    import useEffect
+
+    EffectCallback(함수)과 React.DependencyList(배열)가 필요
+   */
+  useEffect(() => {}, []);
+
   return <div className="bg-blue-100 h-96">FunctionalComponent</div>;
 };
 
