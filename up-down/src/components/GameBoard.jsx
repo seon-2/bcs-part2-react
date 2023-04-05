ab6d6b2521ae2845b7a14504fad87df9a3e2ec95
seon-2 <phia130@naver.com>
2023-04-05 10:22:50 +0900

    💄 [design] GameBoard 화면 디자인

--- up-down/src/components/GameBoard.jsx
+++ up-down/src/components/GameBoard.jsx
@@ -1,6 +1,21 @@
 const GameBoard = () => {
   // grow는 부모가 flex여야 함.
-  return <div className="bg-blue-100 w-full grow">게임보드</div>;
+  return (
+    <div className=" w-full grow flex flex-col justify-center items-center">
+      <div className="mb-4 text-xl font-bold">
+        0 ~ 100 사이의 숫자를 맞춰보세요!
+      </div>
+      <div>
+        <input
+          className="border-2 rounded-lg px-4 py-2 focus:outline-pink-300 shadow-lg"
+          type="text"
+        />
+        <button className="px-4 py-2 ml-2 rounded-lg border-2 border-pink-300 text-pink-300 shadow-lg">
+          확인
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default GameBoard;
