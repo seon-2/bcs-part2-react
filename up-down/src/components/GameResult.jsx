8387d9d964dd4461a28ee1882c046b5eec589e09
seon-2 <phia130@naver.com>
2023-04-05 14:00:00 +0900

    💄 [design] GameResult 화면 디자인

--- up-down/src/components/GameResult.jsx
+++ up-down/src/components/GameResult.jsx
@@ -1,6 +1,14 @@
-const GameResult= () => {
+const GameResult = () => {
+  const point = localStorage.getItem("point");
+  // 현재는 화면 처음 켤 때 현재점수에 아무것도 안 나타남 (0도)
+
   // grow는 부모가 flex여야 함.
-  return <div className="bg-red-100 w-full grow">게임결과</div>;
+  return (
+    <div className="bg-pink-300 w-full grow flex flex-col justify-end items-center text-white pb-8 shadow-pink-300 shadow-lg">
+      <div className="text-8xl font-black">Up & Down</div>
+      <div className="text-2xl">현재 점수 : {point}</div>
+    </div>
+  );
 };
 
 export default GameResult;
