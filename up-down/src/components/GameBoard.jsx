c263f5a261111bbce0b78f8369d077a4dd80da12
seon-2 <phia130@naver.com>
2023-04-05 10:37:40 +0900

    ✨ [feature] 랜덤 숫자 구현 및 추적, 힌트 초기값 설정

--- up-down/src/components/GameBoard.jsx
+++ up-down/src/components/GameBoard.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react";
+
 const GameBoard = () => {
+  // Math.floor는 소수점을 없앰(내림)
+  const [randomNum, setRandomNum] = useState(Math.floor(Math.random() * 100));
+  const [hint, setHint] = useState("0 ~ 100 사이의 숫자를 맞춰보세요!");
+
+  // 랜덤 숫자를 추적
+  useEffect(() => {
+    console.log(`랜덤 숫자는 ${randomNum}입니다.`);
+  }, [randomNum]);
+
   // grow는 부모가 flex여야 함.
   return (
     <div className=" w-full grow flex flex-col justify-center items-center">
-      <div className="mb-4 text-xl font-bold">
-        0 ~ 100 사이의 숫자를 맞춰보세요!
-      </div>
+      <div className="mb-4 text-xl font-bold">{hint}</div>
       <div>
         <input
           className="border-2 rounded-lg px-4 py-2 focus:outline-pink-300 shadow-lg"
