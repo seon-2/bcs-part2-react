741f609a07eeee11a4f5c8dbca77165b9a41865f
seon-2 <phia130@naver.com>
2023-04-06 10:14:21 +0900

    💄 [design] chakra-ui 버튼 isLoading 적용

--- chakra-ui/src/App.js
+++ chakra-ui/src/App.js
@@ -1,12 +1,20 @@
 import { ChakraProvider, Box, Button } from "@chakra-ui/react";
 import { PhoneIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <ChakraProvider>
       <Box color="red.400">Hello, chakra-ui!</Box>
       {/* variant : 변형 스타일*/}
-      <Button colorScheme="cyan" leftIcon={<PhoneIcon />} variant="outline">
+      <Button
+        isLoading={isLoading}
+        colorScheme="cyan"
+        leftIcon={<PhoneIcon />}
+        variant="outline"
+      >
         버 튼
       </Button>
     </ChakraProvider>
