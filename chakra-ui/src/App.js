d83c35c5a31953560a7cd1d6c6db14dad5af85d6
seon-2 <phia130@naver.com>
2023-04-06 10:02:46 +0900

    💄 [design] chakra-ui 사용해보기_Box, Button

--- chakra-ui/src/App.js
+++ chakra-ui/src/App.js
@@ -1,5 +1,12 @@
+import { ChakraProvider, Box, Button } from "@chakra-ui/react";
+
 function App() {
-  return <div>Hello, React!</div>;
+  return (
+    <ChakraProvider>
+      <Box color="red.400">Hello, chakra-ui!</Box>
+      <Button colorScheme="cyan">버 튼</Button>
+    </ChakraProvider>
+  );
 }
 
 export default App;
