7079848dfa16145c1fa14840391d295455088df0
seon-2 <phia130@naver.com>
2023-04-06 13:21:52 +0900

    ✨ [feature] sort로 데이터 shuffle

--- worldcup/src/pages/worldcup.jsx
+++ worldcup/src/pages/worldcup.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import animalData from "../animalData.json";
+
 const Worldcup = () => {
+  const [shuffleAnimal, setShuffleAnimal] = useState();
+
+  useEffect(() => {
+    let shuffleAnimalData = animalData.sort(() => {
+      // 함수 안 넣으면(또는 양수) : 아스키코드 기준 가나다 정렬, 음수 : 역순으로 출력
+      // -0.5~0.5 사이 숫자, 랜덤하게 정렬
+      return Math.random() - 0.5;
+    });
+
+    console.log(shuffleAnimalData);
+  }, []);
+  useEffect(() => console.log(animalData), []);
   return <div>Worldcup</div>;
 };
 
