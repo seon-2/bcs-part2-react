99943a39f1a04cf2c168d00608defbed5cc1bbda
seon-2 <phia130@naver.com>
2023-04-06 13:59:10 +0900

    💄 [design] AnimalCard 디자인

--- worldcup/src/components/AnimalCard.jsx
+++ worldcup/src/components/AnimalCard.jsx
@@ -1,7 +1,13 @@
-const AnimalCard = ({animal}) => {
+const AnimalCard = ({ animal }) => {
   return (
-    <div>
-      <img src={`${process.env.PUBLIC_URL}/images/${animal}.jpeg`} alt="동물" />
+    <div className="flex flex-col justify-normal items-center">
+      <img
+        className="border-8 border-white shadow-lg shadow-white w-96"
+        src={`${process.env.PUBLIC_URL}/images/${animal}.jpeg`}
+        alt="동물"
+      />
+      <div className="text-2xl mt-4 font-bold">{animal} 💕💕💕</div>
+      <button className="text-2xl mt-4 px-4 py-2 rounded-md">선택</button>
     </div>
   );
 };
