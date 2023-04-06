27854bbe670a2128facf91a3d1d1ca87dc427898
seon-2 <phia130@naver.com>
2023-04-06 09:10:54 +0900

    🎉 [start] 프로젝트 시작_use-context

---
+++ use-context/README.md
@@ -0,0 +1,1 @@
+# Create React App with Chakra UI
---
+++ use-context/src/App.jsx
@@ -0,0 +1,11 @@
+import { Box, ChakraProvider } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Box>Hello, React</Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
