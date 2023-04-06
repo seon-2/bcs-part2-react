1ac00bd6fced8099d718cd8f470aff7cab503724
seon-2 <phia130@naver.com>
2023-04-06 10:05:18 +0900

    💄 [design] chakra-ui icon 적용

--- chakra-ui/src/App.js
+++ chakra-ui/src/App.js
@@ -1,10 +1,13 @@
 import { ChakraProvider, Box, Button } from "@chakra-ui/react";
+import { PhoneIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
     <ChakraProvider>
       <Box color="red.400">Hello, chakra-ui!</Box>
-      <Button colorScheme="cyan">버 튼</Button>
+      <Button colorScheme="cyan" leftIcon={<PhoneIcon />}>
+        버 튼
+      </Button>
     </ChakraProvider>
   );
 }
