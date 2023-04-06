d6ed10d38b791e4576a5b857776625271dfc319a
seon-2 <phia130@naver.com>
2023-04-06 10:28:35 +0900

    💄 [design] chakra-ui 사용해보기_Menu

--- chakra-ui/src/App.js
+++ chakra-ui/src/App.js
@@ -1,5 +1,15 @@
-import { ChakraProvider, Box, Button } from "@chakra-ui/react";
-import { PhoneIcon } from "@chakra-ui/icons";
+import {
+  ChakraProvider,
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuGroup,
+  MenuDivider
+} from "@chakra-ui/react";
+import { PhoneIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import { useState } from "react";
 
 function App() {
@@ -17,6 +27,34 @@ function App() {
       >
         버 튼
       </Button>
+      <Menu>
+        <MenuButton
+          colorScheme="teal"
+          as={Button}
+          rightIcon={<ChevronDownIcon />}
+        >
+          저녁 메뉴
+        </MenuButton>
+        <MenuList>
+          <MenuGroup title="파스타">
+            <MenuItem>알리오올리오</MenuItem>
+            <MenuItem>크림파스타</MenuItem>
+            <MenuItem>토마토파스타</MenuItem>
+          </MenuGroup>
+          <MenuDivider />
+          <MenuGroup title="치킨">
+            <MenuItem>양념치킨</MenuItem>
+            <MenuItem>후라이드</MenuItem>
+            <MenuItem>구운치킨</MenuItem>
+          </MenuGroup>
+          <MenuDivider />
+          <MenuGroup title="피자">
+            <MenuItem>파인애플피자</MenuItem>
+            <MenuItem>페퍼로니피자</MenuItem>
+            <MenuItem>치즈피자</MenuItem>
+          </MenuGroup>
+        </MenuList>
+      </Menu>
     </ChakraProvider>
   );
 }
