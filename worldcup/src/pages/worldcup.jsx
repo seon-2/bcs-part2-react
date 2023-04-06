a70634fbcd961d36be37201bace9e07efc15dd96
seon-2 <phia130@naver.com>
2023-04-06 13:53:29 +0900

    🌷 [explain] 에러해결_사이드 이펙트 설명

--- worldcup/src/pages/worldcup.jsx
+++ worldcup/src/pages/worldcup.jsx
@@ -18,7 +18,11 @@ const Worldcup = () => {
   useEffect(() => console.log(animalData), []);
   return (
     <div className="bg-pink-200 min-h-screen flex justify-center items-center">
-      <AnimalCard animal={shuffleAnimal[0]} />
+      {/* 에러 발생 Cannot read properties of undefined (reading '7') */}
+      {/* 사이드 이펙트 */}
+      {/* 렌더링 되는 시점에서 useEffect는 아직 실행 안됨. 데이터가 없음. useState 기준으로 초기값 필요
+      useEffect는 사이드에서 결과값만 넘겨줌 */}
+      {shuffleAnimal && <AnimalCard animal={shuffleAnimal[0]} />}
     </div>
   );
 };
