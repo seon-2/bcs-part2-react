439e27aa4957c2b3706de7956ed695e31bc5b3ae
seon-2 <phia130@naver.com>
2023-04-10 15:12:15 +0900

    ✨ [feature] axios.post 요청 구현_비동기

--- react-icons/src/components/Chat.jsx
+++ react-icons/src/components/Chat.jsx
@@ -1,17 +1,36 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const Chat = () => {
   const [question, setQuestion] = useState("");
 
   // form 태그의 onSubmit 안에 들어가서, 엔터로 검색 시 새로고침 막음.
-  const onSubmitChat = (e) => {
-    e.preventDefault();
+  const onSubmitChat = async (e) => {
+    try {
+      e.preventDefault();
+      // axios.post 요청 : url, body, headers - auth 토큰 설정
+      const response = await axios.post(
+        "https://holy-fire-2749.fly.dev/chat",
+        {
+          question: `${question}`,
+          //   question: question, 같은 표현
+        },
+        {
+          headers: {
+            Authorization: "Bearer BLOCKCHAINSCHOOL3",
+          },
+        }
+      );
+      console.log(response); // 데이터 콘솔로 출력되는지 확인
+    } catch (error) {
+      console.error(error);
+    }
   };
   // cf. 잘 받아와지는지 확인
-  useEffect(() => {
-    console.log(question);
-  }, [question]);
-  
+  //   useEffect(() => {
+  //     console.log(question);
+  //   }, [question]);
+
   return (
     <div className="bg-black min-h-screen flex justify-center items-center text-white">
       <form onSubmit={onSubmitChat}>
