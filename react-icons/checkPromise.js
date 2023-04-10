9349fa7755242c3b7f9bb54c00a09387b7b921ab
seon-2 <phia130@naver.com>
2023-04-10 10:50:44 +0900

    🌷 [explain] async await + try catch

--- react-icons/checkPromise.js
+++ react-icons/checkPromise.js
@@ -2,15 +2,20 @@ async function checkPromise() {
   // 실행이 되면 resolve, 실행이 안되면 reject
   // var Promise: PromiseConstructor
   // new <any>(executor: (resolve: (value: any) => void, reject: (reason?: any) => void) => void) => Promise<any>
-  let promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("완료"), 1000);
-  });
+  try {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve("완료"), 1000);
+    });
 
-  let result = await promise;
-  console.log(result);
+    let result = await promise;
+
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 checkPromise();
 // >> 완료
-// promise 를 가지고 있는 함수에 async를 붙여주고 promise에 await 붙임.
-// 1초 기다렸다가 resolve에 "완료" 가 담기고 그게 result에 담겨서 console 출력
+// async await는 try catch와 같이 잘 쓰임
+// 정상적인 상황은 try문 실행, 에러가 발생하면 catch로 빼냄
