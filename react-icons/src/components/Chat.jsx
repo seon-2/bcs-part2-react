251c74d26200e285c889505e8541b19beebe7f2a
seon-2 <phia130@naver.com>
2023-04-10 14:44:38 +0900

    ✨ [feature] input 창에서 입력한 데이터 가져오기_onChange

--- react-icons/src/components/Chat.jsx
+++ react-icons/src/components/Chat.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
+
 const Chat = () => {
+  const [question, setQuestion] = useState("");
+
   // form 태그의 onSubmit 안에 들어가서, 엔터로 검색 시 새로고침 막음.
   const onSubmitChat = (e) => {
     e.preventDefault();
   };
+  // cf. 잘 받아와지는지 확인
+  useEffect(() => {
+    console.log(question);
+  }, [question]);
+  
   return (
     <div className="bg-black min-h-screen flex justify-center items-center text-white">
       <form onSubmit={onSubmitChat}>
-        <input type="text" className="text-black" />
+        <input
+          type="text"
+          className="text-black"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        />
         <input type="submit" value="검 색" />
       </form>
     </div>
