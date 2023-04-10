b18ce5e77a9792a1889eac56cbedd140a5321b03
seon-2 <phia130@naver.com>
2023-04-10 16:26:04 +0900

    🌷 [explain] CommonJS 사용해보기

---
+++ express/a.js
@@ -0,0 +1,6 @@
+function goModule() {
+  console.log("Hello, Module!");
+}
+
+// export default goModule 과 동일한 기능 ("exports" s가 들어감!!)
+module.exports = goModule;
---
+++ express/b.js
@@ -0,0 +1,4 @@
+// import
+const goModule = require("./a");
+
+goModule();
