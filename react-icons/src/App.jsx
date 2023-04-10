457fe9d8c5634d557ba837963b1c827ad5970681
seon-2 <phia130@naver.com>
2023-04-10 09:46:53 +0900

    💄 [design] react icon 적용

--- react-icons/src/App.jsx
+++ react-icons/src/App.jsx
@@ -1,5 +1,16 @@
+import { FcCloseUpMode } from "react-icons/fc";
+import { BsFillAirplaneEnginesFill } from "react-icons/bs";
+// https://react-icons.github.io/react-icons 에서 원하는 아이콘 찾아서 경로 import
+// 원하는 icon 이름 그대로! 가져오기
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      {/* 사이즈, 색상 설정 */}
+      <FcCloseUpMode size={100} />
+      <BsFillAirplaneEnginesFill size={100} color="lightblue" />
+    </div>
+  );
 }
 
 export default App;
