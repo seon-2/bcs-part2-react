5441848311445d1e52e123b57fabf07f28037659
seon-2 <phia130@naver.com>
2023-04-10 14:34:54 +0900

    ✨ [feature] form 태그의 onSubmit 새로고침 막기

--- react-icons/src/components/Chat.jsx
+++ react-icons/src/components/Chat.jsx
@@ -1,7 +1,11 @@
 const Chat = () => {
+  // form 태그의 onSubmit 안에 들어가서, 엔터로 검색 시 새로고침 막음.
+  const onSubmitChat = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="bg-black min-h-screen flex justify-center items-center text-white">
-      <form>
+      <form onSubmit={onSubmitChat}>
         <input type="text" className="text-black" />
         <input type="submit" value="검 색" />
       </form>
