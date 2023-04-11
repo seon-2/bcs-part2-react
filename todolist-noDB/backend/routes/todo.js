c6577d1779939de1e7d8d9867e1c27060ab36b56
seon-2 <phia130@naver.com>
2023-04-11 13:43:13 +0900

    ✨ [feature] 전체 todo list 조회 구현

--- todolist-noDB/backend/routes/todo.js
+++ todolist-noDB/backend/routes/todo.js
@@ -6,5 +6,12 @@ let todoData = require("../todoData.json");
 // 라우터 설정
 const router = express.Router();
 
+// 전체 todo list 조회 - READ (http://localhost:3010/todo)
+router.get("/", (req, res) => {
+  console.log(todoData);
+
+  res.json(todoData); // json 형식
+});
+
 // exports 필요
 module.exports = router;
