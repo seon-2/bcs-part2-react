7443b8b46399ccf0d43fd7153327e8aeebc26426
seon-2 <phia130@naver.com>
2023-04-11 11:48:28 +0900

    ✨ [feature] express 초기 설정
    포트 열고 서버 시작

---
+++ todolist-noDB/backend/app.js
@@ -0,0 +1,19 @@
+// import 와 같은 기능
+const express = require("express");
+
+// express를 사용
+const app = express();
+
+// 포트번호 설정
+const port = 3010;
+
+// get 요청은 2가지 인자 필요 : 주소와 (요청, 응답)
+// get 요청은 크롬 등 인터넷에 http://localhost:3010 로 접근해서 확인 가능
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+// 서버 시작하는 명령어 2가지 인자 필요 : 포트번호, 화살표 함수 안에 서버가 실행될 때 실행될 내용
+app.listen(port, () => {
+  console.log(`Server listening on port ${port} 👂`);
+});
