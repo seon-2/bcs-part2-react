9db0155f4e00f05ddcb267a8c9cf35383a7c3ec7
seon-2 <phia130@naver.com>
2023-04-11 10:35:58 +0900

    🌷 [explain] 동적라우팅 req.params console에서 확인
    http://localhost:3010/tweet/1 으로 요청 ->
    { id: '1' } 콘솔에 찍힘

--- express-2/routes/tweet.js
+++ express-2/routes/tweet.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 // app.js 에서 app -> router
 router.get("/:id", (req, res) => {
+  console.log(req.params);
+  // http://localhost:3010/tweet/1 으로 요청 -> { id: '1' } vscode 콘솔에 찍힘
+  // http://localhost:3010/tweet/2 으로 요청 -> { id: '2' } vscode 콘솔에 찍힘
   res.send("특정 트윗 조회");
 });
 
