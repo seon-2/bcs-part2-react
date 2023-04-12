3e48350a84a088a3a69780a4287802efb85e2ba9
seon-2 <phia130@naver.com>
2023-04-12 15:50:21 +0900

    ✨ [feature] axios.delete 요청 구현

--- todolist-noDB/frontend/src/components/TodoCard.jsx
+++ todolist-noDB/frontend/src/components/TodoCard.jsx
@@ -21,6 +21,23 @@ const TodoCard = ({ title, isDone, index, getToDoList }) => {
     }
   };
 
+  const onClickDelete = async () => {
+    try {
+      const response = await axios.delete(
+        `${process.env.REACT_APP_BACKEND_URL}/todo/${index}`
+      );
+
+      if (response.status !== 200) {
+        alert("데이터를 불러오지 못했습니다.");
+        return;
+      }
+
+      getToDoList();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       {isDone ? (
@@ -31,11 +48,13 @@ const TodoCard = ({ title, isDone, index, getToDoList }) => {
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-4 border-white bg-pink-400 w-8 h-8 scale-75 rounded-xl"></div>
           </div>
           <div className="text-2xl ml-4 line-through">{title}</div>
+          <button onClick={onClickDelete}>삭제</button>
         </li>
       ) : (
         <li className="flex my-4" onClick={onClickToggle}>
           <div className="border-4 border-pink-400 w-8 h-8 rounded-xl"></div>
           <div className="text-2xl ml-4 ">{title}</div>
+          <button onClick={onClickDelete}>삭제</button>
         </li>
       )}
     </>
