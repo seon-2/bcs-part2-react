0a6d53ea3f86bfa520d2a8756e39a3d340eb6402
seon-2 <phia130@naver.com>
2023-04-13 14:30:50 +0900

    ✨ [feature] cors, json 미들웨어 추가

--- todolist/backend/app.js
+++ todolist/backend/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const port = 3010;
+
+// 미들웨어 추가
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
