baa77bc29b1d978efb408379732e2f1b0bcfb7e8
seon-2 <phia130@naver.com>
2023-04-13 17:37:21 +0900

    ✨ [feature] user 라우팅 설정 및 연결

--- todolist/backend/app.js
+++ todolist/backend/app.js
@@ -5,9 +5,13 @@ const app = express();
 
 const port = 3010;
 
+// user.js 연결
+const userRouter = require("./routes/user");
+
 // 미들웨어 추가
 app.use(cors());
 app.use(express.json());
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
