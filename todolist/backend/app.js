a724f03443438266a9f3f3148021222af87a42d8
seon-2 <phia130@naver.com>
2023-04-13 14:27:32 +0900

    ✨ [feature] express 초기 설정
    포트 열고 서버 시작

---
+++ todolist/backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3010;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port : ${port} 🌷`);
+});
