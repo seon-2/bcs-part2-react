5c82ad7111bc4afd1d934fe599879ac3a567ab39
seon-2 <phia130@naver.com>
2023-04-14 11:46:03 +0900

    ✨ [feature] todo 조회 구현 (예외처리 포함)

--- todolist/backend/routes/todo.js
+++ todolist/backend/routes/todo.js
@@ -45,4 +45,41 @@ router.post("/", async (req, res) => {
   }
 });
 
+// todo 조회 - GET
+router.get("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    // todo 조회 전, user 있는지 체크
+    const user = await client.user.findMany({
+      where: {
+        id: parseInt(userId),
+      },
+      // 0번부터 3개 출력 (findMany로 조회할 때는 값이 많을 수도 있기 때문에)
+      // 실제 동작할 때는 skip이 take만큼 더해져야 함 (0 -> 3 -> 6 ...)
+      //   skip: 0,
+      //   take: 3,
+    });
+
+    if (!user) {
+      return res.status(400).json({ ok: false, error: "Not exist user." });
+    }
+
+    // 조건을 만족하는(여기에서는 userId가 일치하는) 값이 여러 개일 때 findMany 사용
+    // findUnique는 유일한 값으로 찾음. 스키마에서 @unique로 설정한
+    // 하나만 찾을 때는 findFirst
+    const todos = await client.todo.findMany({
+      where: {
+        userId: parseInt(userId),
+      },
+    });
+
+    // console.log(todos);
+
+    res.json({ ok: true, todos });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 module.exports = router;
