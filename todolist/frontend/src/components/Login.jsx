2e6c11fe851e79fe1185380ff3c66d977e58ce86
seon-2 <phia130@naver.com>
2023-04-14 16:17:54 +0900

    ✨ [feature] user 생성 구현
    DB와 FE 연결

--- todolist/frontend/src/components/Login.jsx
+++ todolist/frontend/src/components/Login.jsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
+import axios from "axios";
+
 const LogIn = () => {
+  const [createAccount, setCreateAccount] = useState("");
+
+  // 유저 생성 - CREATE
+  const onSubmitCreateAccount = async (e) => {
+    try {
+      // 새로고침 방지
+      e.preventDefault();
+
+      // .env 파일 만들어서 REACT_APP_BACKEND_URL 설정 (git에는 안 올림!) .env 생성, 수정 시 FE npm run start 다시
+      const response = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/user`,
+        {
+          account: createAccount,
+        }
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
-      <form className="flex mt-2 my-16">
+      <form className="flex mt-2 my-16" onSubmit={onSubmitCreateAccount}>
         <input
           className="grow border-2 border-pink-200 rounded-lg focus:outline-pink-400 px-2 py-1 text-lg"
           type="text"
+          value={createAccount}
+          onChange={(e) => {
+            setCreateAccount(e.target.value);
+          }}
         />
         <input
           className="ml-4 px-2 py-1 bg-pink-400 rounded-lg text-gray-50 w-24"
