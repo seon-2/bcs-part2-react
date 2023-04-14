ce5267a2543de82f891df66efa6189024c2b6806
seon-2 <phia130@naver.com>
2023-04-14 10:18:43 +0900

    ✨ [feature] todo 라우팅 설정 및 연결

--- todolist/backend/app.js
+++ todolist/backend/app.js
@@ -7,11 +7,14 @@ const port = 3010;
 
 // user.js 연결
 const userRouter = require("./routes/user");
+// todo.js 연결
+const todoRouter = require("./routes/todo");
 
 // 미들웨어 추가
 app.use(cors());
 app.use(express.json());
 app.use("/user", userRouter);
+app.use("/todo", todoRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
