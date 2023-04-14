d6637ac6495a4360e5012d8ac3888dbcbc66b279
seon-2 <phia130@naver.com>
2023-04-14 09:36:46 +0900

    ✨ [feature] user 생성 시 중복 방지 구현

--- todolist/backend/routes/user.js
+++ todolist/backend/routes/user.js
@@ -8,10 +8,26 @@ const client = new PrismaClient();
 
 // user 생성 - POST
 // BE와 통신하기 위해 비동기로. async, await 사용
+// 중복된 account POST 요청 시 에러 발생 (id는 늘어남) Unique constraint failed...
+// 해결하기 위해 아이디 조회 하고 없으면 생성
 router.post("/", async (req, res) => {
   try {
     // req.body에서 account 받아오기
     const { account } = req.body;
+
+    // existUser 가 null일 때 정상 (= 이미 존재하는, 중복된 계정이 없다는 뜻)
+    const existUser = await client.user.findUnique({
+      where: {
+        account,
+      },
+    });
+    // console.log(existUser);
+    // 존재하면 400 코드 던져주기
+    if (existUser) {
+      return res
+        .status(400)
+        .json({ ok: false, error: "Already exist account." });
+    }
 
     // DB와 통신 - prisma client 사용. js도 만든 테이블 자동완성 지원
     const user = await client.user.create({
