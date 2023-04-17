fae821c0aa369fc1537f49dc4bb6a61519432f65
seon-2 <phia130@naver.com>
2023-04-17 13:30:08 +0900

    ✨ [feature] todo 생성 구현 - CREATE

--- todolist/frontend/src/components/CreateTodo.jsx
+++ todolist/frontend/src/components/CreateTodo.jsx
@@ -1,9 +1,36 @@
-const CreateTodo = () => {
+import axios from "axios";
+import { useState } from "react";
+
+// todo 생성 - CREATE
+const CreateTodo = ({ userId }) => {
+  // 입력창에서 받는, 사용자가 작성하는 to 저장하는 변수
+  const [todo, setTodo] = useState("");
+
+  const onSubmitCreateTodo = async (e) => {
+    try {
+      e.preventDefault();
+
+      const response = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/todo`,
+        {
+          todo,
+          userId,
+        }
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <form className="flex mt-2">
+    <form className="flex mt-2" onSubmit={onSubmitCreateTodo}>
       <input
         className="grow border-2 border-pink-200 rounded-lg focus:outline-pink-400 px-2 py-1 text-lg"
         type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
       />
       <input
         className="ml-4 px-2 py-1 bg-pink-400 rounded-lg text-gray-50"
