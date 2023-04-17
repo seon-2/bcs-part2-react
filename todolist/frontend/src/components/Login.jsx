5379c9997e5a87aed22d40101bcc08c8fd6c2acd
seon-2 <phia130@naver.com>
2023-04-17 10:34:27 +0900

    ✨ [feature] 로그인 기능 구현 - GET

--- todolist/frontend/src/components/Login.jsx
+++ todolist/frontend/src/components/Login.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const LogIn = ({ setUser }) => {
   const [createAccount, setCreateAccount] = useState("");
+  const [account, setAccount] = useState("");
 
   // 유저 생성 - CREATE
   const onSubmitCreateAccount = async (e) => {
@@ -29,6 +30,27 @@ const LogIn = ({ setUser }) => {
     }
   };
 
+  // 유저 조회 - GET
+  const onSubmitLogIn = async (e) => {
+    try {
+      // 새로고침 방지
+      e.preventDefault();
+
+      const response = await axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/user/${account}`,
+        {
+          account: createAccount,
+        }
+      );
+
+      // console.log(response);
+      setUser(response.data.user);
+    } catch (error) {
+      console.error(error);
+      alert("로그인을 실패하였습니다.");
+    }
+  };
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
       <form className="flex mt-2 my-16" onSubmit={onSubmitCreateAccount}>
@@ -44,6 +66,21 @@ const LogIn = ({ setUser }) => {
           className="ml-4 px-2 py-1 bg-pink-400 rounded-lg text-gray-50 w-24"
           type="submit"
           value="계정 생성"
+        />
+      </form>
+      <form className="flex mt-2 my-16" onSubmit={onSubmitLogIn}>
+        <input
+          className="grow border-2 border-pink-200 rounded-lg focus:outline-pink-400 px-2 py-1 text-lg"
+          type="text"
+          value={account}
+          onChange={(e) => {
+            setAccount(e.target.value);
+          }}
+        />
+        <input
+          className="ml-4 px-2 py-1 bg-pink-400 rounded-lg text-gray-50 w-24"
+          type="submit"
+          value="로그인"
         />
       </form>
     </div>
