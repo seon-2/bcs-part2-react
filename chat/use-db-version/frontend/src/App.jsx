22a96e218f7cbec3e75172e4523be6ab142f6338
seon-2 <phia130@naver.com>
2023-04-18 14:41:12 +0900

    💄 [design] 화면 디자인

--- chat/use-db-version/frontend/src/App.jsx
+++ chat/use-db-version/frontend/src/App.jsx
@@ -1,5 +1,42 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  const [content, setContent] = useState("");
+  const [result, setResult] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmitChat = async (e) => {
+    try {
+      e.preventDefualt();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="max-w-screen-md mx-auto min-h-screen flex flex-col justify-start items-center pt-16 px-4">
+      <form className="flex w-full" onSubmit={onSubmitChat}>
+        <input
+          className={`grow border-2 px-2 py-1 border-gray-300 rounded-lg focus:outline-main shadow-lg ${
+            isLoading && "bg-gray-200 text-gray-400"
+          }`}
+          type="text"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          disabled={isLoading}
+        />
+        <input
+          className={`w-24 ml-4 px-2 py-1 border-2 border-main text-main rounded-lg shadow-lg ${
+            isLoading && "bg-main text-gray-200"
+          }`}
+          type="submit"
+          disabled={isLoading}
+          value={isLoading ? "검색중..." : "검색"}
+        />
+      </form>
+      {result && <div className="mt-16 bg-main p-4 text-gray-50">{result}</div>}
+    </div>
+  );
 }
 
 export default App;
