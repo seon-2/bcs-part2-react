47fed92bf08c64c3278d9409bae2f25011348a9a
seon-2 <phia130@naver.com>
2023-04-18 13:58:12 +0900

    ✨ [feature] chat post 코드 작성 - 질문 없는 경우 예외처리

--- chat/use-db-version/backend/app.js
+++ chat/use-db-version/backend/app.js
@@ -17,12 +17,18 @@ app.get("/", (req, res) => {
 // 비동기 함수 openAI와의 통신을 기다림
 app.post("/chat", async (req, res) => {
   try {
+    const { content } = req.body;
     const bearerToken = req.headers.authorization?.substring(7);
 
+    // 키 입력, 유무 확인
     if (bearerToken !== process.env.SECRET_KEY) {
       return res
         .status(400)
         .json({ ok: false, error: "올바른 키를 입력해주세요." });
+    }
+    // body 안 보낸 경우 예외 처리
+    if (!content) {
+      return res.status(400).json({ ok: false, error: "질문을 입력해주세요." });
     }
 
     res.send("임시");
