cb2d9806fc9336af37c4768bd6c7d075585ebe7d
seon-2 <phia130@naver.com>
2023-04-18 10:41:23 +0900

    ✨ [feature] 질문 보내기 axios.post 코드 작성, UI에 연결

--- chat/no-db-version/src/App.jsx
+++ chat/no-db-version/src/App.jsx
@@ -1,10 +1,42 @@
+import axios from "axios";
+import { useState } from "react";
+
 function App() {
+  const [content, setContent] = useState("");
+
+  // gpt에 질문 보내기 - POST
+  const onSubmitChat = async (e) => {
+    try {
+      e.preventDefault();
+
+      const response = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content }],
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.REACT_APP_OPENAI_KEY}`,
+          },
+        }
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="max-w-screen-md mx-auto min-h-screen flex flex-col justify-start items-center pt-16 px-4">
-      <form className="flex w-full">
+      <form className="flex w-full" onSubmit={onSubmitChat}>
         <input
           className="grow border-2 border-gray-300 rounded-lg focus:outline-main shadow-lg"
           type="text"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
         />
         <input
           className="ml-4 px-2 py-1 border-2 border-main text-main rounded-lg shadow-lg"
