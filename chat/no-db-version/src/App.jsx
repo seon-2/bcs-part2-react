c57abbfaf5c8f3daa7446437ae97411b4b8b6809
seon-2 <phia130@naver.com>
2023-04-18 10:01:29 +0900

    💄 [design] 입력 화면 디자인

--- chat/no-db-version/src/App.jsx
+++ chat/no-db-version/src/App.jsx
@@ -1,5 +1,18 @@
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  return (
+    <div className="max-w-screen-md mx-auto min-h-screen flex flex-col justify-start items-center pt-16 px-4">
+      <form className="flex w-full">
+        <input
+          className="grow border-2 border-gray-300 rounded-lg focus:outline-main shadow-lg"
+          type="text"
+        />
+        <input
+          className="ml-4 px-2 py-1 border-2 border-main text-main rounded-lg shadow-lg"
+          type="submit"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default App;
