ea3a61a0f23df83ec593e88ae664651a30b5ac3a
seon-2 <phia130@naver.com>
2023-04-18 13:54:40 +0900

    ✨ [feature] chat post 코드 작성 - authorization 비교 적용

--- chat/use-db-version/backend/app.js
+++ chat/use-db-version/backend/app.js
@@ -17,13 +17,13 @@ app.get("/", (req, res) => {
 // 비동기 함수 openAI와의 통신을 기다림
 app.post("/chat", async (req, res) => {
   try {
-    // key 비교 (authorization에 Bearer라는 글자가 포함되어있어서 바로 비교 불가)
-    // if문 대신에 optional chaining 사용
     const bearerToken = req.headers.authorization?.substring(7);
 
-    console.log(bearerToken);
-    console.log(process.env.SECRET_KEY);
-    console.log(bearerToken === process.env.SECRET_KEY);
+    if (bearerToken !== process.env.SECRET_KEY) {
+      return res
+        .status(400)
+        .json({ ok: false, error: "올바른 키를 입력해주세요." });
+    }
 
     res.send("임시");
   } catch (error) {
