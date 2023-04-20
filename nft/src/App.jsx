4808ec6f543b7bbaac5be6916735663934415fd5
seon-2 <phia130@naver.com>
2023-04-20 14:02:31 +0900

    ✨ [feature] 지갑로그인 - useState, 삼항연산자

--- nft/src/App.jsx
+++ nft/src/App.jsx
@@ -1,20 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [account, setAccount] = useState("");
+
   // 메타마스크 있을 때 Proxy 출력, 없으면 undefined
-  useEffect(() => {
-    console.log(window.ethereum);
-  }, []);
+  // useEffect(() => {
+  //   console.log(window.ethereum);
+  // }, []);
 
   // 메타마스크 지갑 연결
   const onClickAccount = async () => {
     try {
-      // 지갑 주소 가져오기 
+      // 지갑 주소 가져오기
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
 
-      console.log(accounts);
+      // console.log(accounts);
+      setAccount(accounts[0]);
     } catch (error) {
       console.error(error);
     }
@@ -22,13 +25,17 @@ function App() {
 
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
-      <button className="btn-style" onClick={onClickAccount}>
-        <img
-          className="w-12"
-          src={`${process.env.PUBLIC_URL}/images/metamask.png`}
-          alt="metamask"
-        />
-      </button>
+      {account ? (
+        account
+      ) : (
+        <button className="btn-style" onClick={onClickAccount}>
+          <img
+            className="w-12"
+            src={`${process.env.PUBLIC_URL}/images/metamask.png`}
+            alt="metamask"
+          />
+        </button>
+      )}
     </div>
   );
 }
