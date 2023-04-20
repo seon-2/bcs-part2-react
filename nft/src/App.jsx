eab98d42f4e548520a145a1ecf7163364179411a
seon-2 <phia130@naver.com>
2023-04-20 14:39:23 +0900

    ✨ [feature] 지갑 로그아웃 - useState

--- nft/src/App.jsx
+++ nft/src/App.jsx
@@ -23,11 +23,22 @@ function App() {
     }
   };
 
+  // 로그아웃 기능 (완전히 연결 끊는 것은 아님)
+  const onClickLogOut = () => {
+    setAccount("");
+  };
+  <button className="ml-4 btn-style" onClick={onClickLogOut}>
+    로그아웃
+  </button>;
+
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
       {account ? (
         <div className="text-main font-semibold">
           {account.substring(0, 4)}...{account.substring(account.length - 4)}
+          <button className="ml-4 btn-style" onClick={onClickLogOut}>
+            로그아웃
+          </button>
         </div>
       ) : (
         <button className="btn-style" onClick={onClickAccount}>
