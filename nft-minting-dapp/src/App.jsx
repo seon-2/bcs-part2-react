67187cce64f0b7ff86491ce045c363677cead98f
seon-2 <phia130@naver.com>
2023-04-24 17:43:38 +0900

    ✨ [feature] 지갑로그인 - useState, 삼항연산자

--- nft-minting-dapp/src/App.jsx
+++ nft-minting-dapp/src/App.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  // 지갑주소 받는 변수
+  const [account, setAccount] = useState("");
+
+  // 지갑 연결(로그인)
+  const onClickAccount = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="bg-red-100 min-h-screen flex justify-center items-center">
+      {/* account 있으면 account 화면에 출력, 아니면 로그인 버튼 화면에 출력 */}
+      {account ? (
+        <div>{account}</div>
+      ) : (
+        <button onClick={onClickAccount}>지갑로그인</button>
+      )}
+    </div>
+  );
 }
 
 export default App;
