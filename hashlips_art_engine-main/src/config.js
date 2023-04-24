69240fe6ff28bbd284041f12776237a07cc37553
seon-2 <phia130@naver.com>
2023-04-24 15:31:26 +0900

    🔧 [config] layers 설정 후 조합된 이미지 만들기
    npm run build

--- hashlips_art_engine-main/src/config.js
+++ hashlips_art_engine-main/src/config.js
@@ -24,15 +24,12 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 6,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Face" },
+      { name: "Hat" },
+      { name: "Eyes" },
     ],
   },
 ];
