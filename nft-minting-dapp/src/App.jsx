988221ad2f82ab598f3c8ade1e27aefadb666f4d
seon-2 <phia130@naver.com>
2023-04-24 18:04:49 +0900

    ✨ [feature] nft 민팅 기능 구현

--- nft-minting-dapp/src/App.jsx
+++ nft-minting-dapp/src/App.jsx
@@ -1,4 +1,11 @@
 import { useState } from "react";
+import Web3 from "web3";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "./web3.config";
+
+const web3 = new Web3(window.ethereum);
+// contract 선언 (abi, ca) 순서
+const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+// console.log(contract);
 
 function App() {
   // 지갑주소 받는 변수
@@ -17,6 +24,17 @@ function App() {
     }
   };
 
+  // nft 민팅
+  const onClickMint = async () => {
+    try {
+      const mintNft = await contract.methods.mintNft().send({ from: account });
+
+      console.log(mintNft);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
       {/* account 있으면 account 화면에 출력, 아니면 로그인 버튼 화면에 출력 */}
@@ -24,6 +42,7 @@ function App() {
         // 앞 뒤에 4글자, 나머지는 ...
         <div>
           {account.substring(0, 4)}...{account.substring(account.length - 4)}
+          <button onClick={onClickMint}>민팅</button>
         </div>
       ) : (
         <button onClick={onClickAccount}>지갑로그인</button>
