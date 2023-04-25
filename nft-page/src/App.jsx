646e66538de00696ee7700c222112fad3c727932
seon-2 <phia130@naver.com>
2023-04-25 10:48:57 +0900

    💄 [design] 페이지 만들고 Router 적용 - 다이나믹라우팅

--- nft-page/src/App.jsx
+++ nft-page/src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./pages/main";
+import Detail from "./pages/detail";
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  return (
+    <BrowserRouter>
+      <div className="bg-red-100">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/:tokenId" element={<Detail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
