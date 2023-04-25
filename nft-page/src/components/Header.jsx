8f826ed9acfbdf908ee9251850958ae62ec61079
seon-2 <phia130@naver.com>
2023-04-25 11:57:12 +0900

    💄 [design] 헤더 지갑 주소 UI 디자인

--- nft-page/src/components/Header.jsx
+++ nft-page/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { FaChessRook } from "react-icons/fa";
 import { BiWallet } from "react-icons/bi";
+import { AiFillHeart } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
 const Header = ({ account, setAccount }) => {
@@ -26,7 +27,15 @@ const Header = ({ account, setAccount }) => {
       </Link>
       <div>
         {account ? (
-          <div>{account}</div>
+          <div className="flex items-center p-2 bg-gray-800 rounded-full">
+            <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
+              <AiFillHeart />
+            </div>
+            <div className="ml-1">
+              {account.substring(0, 4)}...
+              {account.substring(account.length - 4)}
+            </div>
+          </div>
         ) : (
           <button
             className="flex items-center p-2 bg-gray-800 rounded-full"
