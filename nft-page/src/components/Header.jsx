f0fac265c2901b853ebfcc8809d4be26c1292862
seon-2 <phia130@naver.com>
2023-04-25 11:27:30 +0900

    ✨ [feature] 헤더 로고 누르면 메인으로 이동하는 기능 구현

--- nft-page/src/components/Header.jsx
+++ nft-page/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import { FaChessRook } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="max-w-screen-xl mx-auto p-4 flex justify-between items-center font-bold">
-      <div className="flex items-center text-main">
-        <FaChessRook size={28} />
-        <div className="ml-1 text-xl">Ble-Chess-3rd</div>
-      </div>
+      <Link to="/">
+        <div className="flex items-center text-main">
+          <FaChessRook size={28} />
+          <div className="ml-1 text-xl">Ble-Chess-3rd</div>
+        </div>
+      </Link>
       <div>wallet</div>
     </header>
   );
