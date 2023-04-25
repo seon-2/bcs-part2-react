2f2faa61a72ac8a46f0984c3c55811ed18feb852
seon-2 <phia130@naver.com>
2023-04-25 11:55:24 +0900

    ✨ [feature] 지갑 로그인 기능 구현

--- nft-page/src/App.jsx
+++ nft-page/src/App.jsx
@@ -2,12 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/main";
 import Detail from "./pages/detail";
 import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
+  // App.jsx에서 account 선언. 지갑 주소를 props로 내려서 다른 페이지에서도 사용할 수 있도록!
+  const [account, setAccount] = useState("");
+
   return (
     <BrowserRouter>
-      <div className="bg-red-100">
-        <Header />
+      <div className="min-h-screen bg-gray-950 text-white">
+        <Header account={account} setAccount={setAccount} />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/:tokenId" element={<Detail />} />
--- nft-page/src/components/Header.jsx
+++ nft-page/src/components/Header.jsx
@@ -2,7 +2,20 @@ import { FaChessRook } from "react-icons/fa";
 import { BiWallet } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = ({ account, setAccount }) => {
+  // 지갑 로그인 기능
+  const onClickAccount = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <header className="max-w-screen-xl mx-auto p-4 flex justify-between items-center font-bold">
       <Link to="/">
@@ -12,12 +25,19 @@ const Header = () => {
         </div>
       </Link>
       <div>
-        <button className="flex items-center p-2 bg-gray-800 rounded-full">
-          <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
-            <BiWallet />
-          </div>
-          <div className="ml-1">Connect</div>
-        </button>
+        {account ? (
+          <div>{account}</div>
+        ) : (
+          <button
+            className="flex items-center p-2 bg-gray-800 rounded-full"
+            onClick={onClickAccount}
+          >
+            <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
+              <BiWallet />
+            </div>
+            <div className="ml-1">Connect</div>
+          </button>
+        )}
       </div>
     </header>
   );
