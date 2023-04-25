c8a46295751f02522599d58001585acef77ab6ea
seon-2 <phia130@naver.com>
2023-04-25 14:35:06 +0900

    💄 [design] Intro UI 디자인 - 배경, 랜덤 이미지

--- nft-page/src/components/Intro.jsx
+++ nft-page/src/components/Intro.jsx
@@ -1,5 +1,24 @@
+// 1~1000까지의 랜덤숫자 생성 (Math.random()은 0~1사이 숫자 랜덤으로)
+const ranNum = Math.floor(Math.random() * 1000) + 1;
+const imgSrc = `${process.env.REACT_APP_IMAGE_URL}/${ranNum}.png`;
+
 const Intro = () => {
-  return <div>Intro</div>;
+  return (
+    <div className="bg-gradient-to-b from-transparent to-red-400 pt-10 ">
+      <div className="max-w-screen-xl mx-auto">
+        <div className="relative">
+          <img
+            className="absolute w-40 h-40 rounded-full"
+            src={imgSrc}
+            alt="NFT"
+          />
+          <div className="w-40 h-40 rounded-full bg-white text-gray-950 flex justify-center items-center">
+            Loading...
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
