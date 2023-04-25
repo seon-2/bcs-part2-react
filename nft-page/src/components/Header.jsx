b11ddf8459cfe967f4a18cc8b45f218ca0a5b028
seon-2 <phia130@naver.com>
2023-04-25 11:23:06 +0900

    💄 [design] 헤더 UI 디자인 - 로고

--- nft-page/src/components/Header.jsx
+++ nft-page/src/components/Header.jsx
@@ -1,5 +1,15 @@
+import { FaChessRook } from "react-icons/fa";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="max-w-screen-xl mx-auto p-4 flex justify-between items-center font-bold">
+      <div className="flex items-center text-main">
+        <FaChessRook size={28} />
+        <div className="ml-1 text-xl">Ble-Chess-3rd</div>
+      </div>
+      <div>wallet</div>
+    </header>
+  );
 };
 
 export default Header;
