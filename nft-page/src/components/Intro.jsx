603f6143df6f372577e619015aea87a5e3374066
seon-2 <phia130@naver.com>
2023-04-25 14:50:34 +0900

    💄 [design] Intro UI 디자인 - 설명, CA가져오기

--- nft-page/src/components/Intro.jsx
+++ nft-page/src/components/Intro.jsx
@@ -1,3 +1,6 @@
+import { FaChessRook } from "react-icons/fa";
+import { CONTRACT_ADDRESS } from "../web3.config";
+
 // 1~1000까지의 랜덤숫자 생성 (Math.random()은 0~1사이 숫자 랜덤으로)
 const ranNum = Math.floor(Math.random() * 1000) + 1;
 const imgSrc = `${process.env.REACT_APP_IMAGE_URL}/${ranNum}.png`;
@@ -15,6 +18,22 @@ const Intro = () => {
           <div className="w-40 h-40 rounded-full bg-white text-gray-950 flex justify-center items-center">
             Loading...
           </div>
+        </div>
+        <div className="mt-4 text-2xl font-bold flex items-center">
+          Da Den Bu
+          <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center ml-4 text-gray-950">
+            <FaChessRook size={16} />
+          </div>
+        </div>
+        <div className="mt-2 flex items-center text-gray-300">
+          by
+          <div className="text-main ml-2">{CONTRACT_ADDRESS}</div>
+        </div>
+        <div className="mt-2 text-gray-300">
+          다덴부란(.env란 "environment"의 약어)은 소프트웨어 개발에서 자주
+          사용되는 파일 형식 중 하나입니다. 이 파일은 소프트웨어 개발자들이
+          프로젝트에서 사용되는 환경 변수(environment variable)를 저장하는 데
+          사용됩니다.
         </div>
       </div>
     </div>
