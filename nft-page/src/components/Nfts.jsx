807b76f885946e809027980b08b221490d137c14
seon-2 <phia130@naver.com>
2023-04-25 17:32:09 +0900

    ✨ [feature] 페이지 선택 기능 구현

--- nft-page/src/components/Nfts.jsx
+++ nft-page/src/components/Nfts.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import { useEffect } from "react";
 
 const Nfts = ({ page }) => {
+  // 현재 선택된 페이지 담는 변수
+  const [selectedPage, setSelectedPage] = useState(1);
+
+  // 클릭 이벤트와 selectedPage 연결. 이차함수
+  // onClickPage = (p) => {} 이렇게 넣으면 안되는 이유는 p에 event가 들어가기 때문!
+  const onClickPage = (p) => () => {
+    setSelectedPage(p);
+  };
+
   // page 잘 받아오는지 확인. 의존성배열 빈값으로 두면, useState 초기값만 들어옴
   useEffect(() => {
     console.log(page);
@@ -12,8 +22,14 @@ const Nfts = ({ page }) => {
 
     for (let i = 0; i < page; i++) {
       pageArray.push(
-        <button key={i}>
-          {i + 1} <span>페이지</span>
+        <button
+          key={i}
+          className={`ml-4 text-2xl font-bold hover:text-white ${
+            i + 1 === selectedPage ? "text-white" : "text-gray-400"
+          }`}
+          onClick={onClickPage(i + 1)}
+        >
+          {i + 1} <span className="text-base">페이지</span>
         </button>
       );
     }
