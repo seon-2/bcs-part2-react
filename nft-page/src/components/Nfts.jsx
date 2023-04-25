0a1728f42863a1a7efdef71740c3078efed00b81
seon-2 <phia130@naver.com>
2023-04-25 18:08:52 +0900

    ✨ [feature] 페이지 별로 nft 데이터 10개씩 불러오기

--- nft-page/src/components/Nfts.jsx
+++ nft-page/src/components/Nfts.jsx
@@ -1,9 +1,41 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import axios from "axios";
 
 const Nfts = ({ page }) => {
   // 현재 선택된 페이지 담는 변수
   const [selectedPage, setSelectedPage] = useState(1);
+  const [nfts, setNfts] = useState();
+
+  // 페이지 별로 nft 10개씩 불러오기
+  const getNfts = async (p) => {
+    try {
+      let nftArray = [];
+
+      for (let i = 0; i < 10; i++) {
+        // (p - 1) * 10 따로 i + 1 따로 생각하면 조금 편합니다.
+        // 페이지 하나에 10개씩 출력해야 합니다. (1페이지에는 1~10, 2페이지에는 11~20)
+        // i는 0~9까지 하나씩 커지니까 1페이지는 i+1만 있어도 구현이 가능합니다.
+        // 같은 식을 사용해 2가 들어올 때 11~20을 만들어주려면 어떻게 하면 좋을까요?
+        // 10+1, 10+2, 10+3, ... 10+10 이렇게 i+1에 10씩 더해주면 됩니다.
+        // 그 다음 페이지는 20, 30 ,,, 계속 i+1에 더해주면 되겠죠?
+        // p를 사용해서 p가 1일 때는 10을 안 더해야 하고, p가 2일 때는 10을 더하고, p가 3일 때는 20을 더하고 ...
+        // 그래서 (p - 1) * 10이라는 식이 세워졌습니다.
+        const tokenId = i + 1 + (p - 1) * 10;
+
+        let response = await axios.get(
+          `${process.env.REACT_APP_JSON_URL}/${tokenId}.json`
+        );
+
+        // console.log(response);
+
+        nftArray.push({ tokenId, metadata: response.data });
+      }
+      setNfts(nftArray);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   // 클릭 이벤트와 selectedPage 연결. 이차함수
   // onClickPage = (p) => {} 이렇게 넣으면 안되는 이유는 p에 event가 들어가기 때문!
@@ -35,6 +67,16 @@ const Nfts = ({ page }) => {
     }
     return pageArray;
   };
+
+  useEffect(() => {
+    // page가 1인 상태 테스트 (1~10번 nft 불러옴)
+    getNfts(1);
+  }, []);
+
+  // tokenId와 metadata가 담긴 nftArray 확인
+  useEffect(() => {
+    console.log(nfts);
+  }, [nfts]);
 
   return (
     <div>
