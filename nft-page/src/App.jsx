f919d917ddd3d5b4e21d51efcbdec3aad6ccae51
seon-2 <phia130@naver.com>
2023-04-25 11:04:12 +0900

    💄 [design] 헤더 컴포넌트 생성

--- nft-page/src/App.jsx
+++ nft-page/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/main";
 import Detail from "./pages/detail";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="bg-red-100">
+        <Header />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/:tokenId" element={<Detail />} />
