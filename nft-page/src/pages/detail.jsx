59e3a8cb5124a9363a18fb3fe4b6c4f6154fc3f5
seon-2 <phia130@naver.com>
2023-04-26 13:12:57 +0900

    💄 [design] Detail 페이지 UI 디자인

--- nft-page/src/pages/detail.jsx
+++ nft-page/src/pages/detail.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { FaChessRook } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 
 const Detail = () => {
@@ -31,7 +32,38 @@ const Detail = () => {
   // metadata 잘 가져오는지 확인
   // useEffect(() => console.log(metadata), [metadata]);
 
-  return <div>{metadata ? "있을때" : <div>로딩중입니다...</div>}</div>;
+  return (
+    <div className="flex flex-col xl:flex-row justify-center items-center py-16 bg-gray-900">
+      {metadata ? (
+        <>
+          <div className="max-w-[512px]">
+            <img className="rounded-t-2xl" src={metadata.image} alt="NFT" />
+            <ul className="grid grid-cols-4 gap-8 py-8 bg-gray-600 rounded-b-2xl text-center">
+              {metadata.attributes.map((v, i) => {
+                return (
+                  <li key={i} className="mx-4">
+                    <div>{v.trait_type}</div>
+                    <div className="mt-1 border-t-2 font-bold">{v.value}</div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="m-8">
+            <div className="text-4xl flex items-center">
+              <div>{metadata.name}</div>
+              <div className="bg-main w-8 h-8 rounded-full flex justify-center items-center ml-2 text-gray-950">
+                <FaChessRook size={18} />
+              </div>
+            </div>
+            <div className="mt-8 text-2xl">{metadata.description}</div>
+          </div>
+        </>
+      ) : (
+        <div>로딩중입니다...</div>
+      )}
+    </div>
+  );
 };
 
 export default Detail;
